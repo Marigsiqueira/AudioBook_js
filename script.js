dffb577437ca76de71fc17a28abf45c140c6281d
marigsiqueira <mgsmariana123@gmail.com>
2024-02-02 00:28:51 -0300

    Idenficando variáveis para o script

---
+++ script.js
@@ -0,0 +1,6 @@
+const botaoPlayPause = document.getElementById('play-pause'); //pegando o valor do botao pelo id e guardando na variável.
+const audioCapitulo = document.getElementById('audio-capitulo');
+
+const numeroCapitulos = 10;
+
+
